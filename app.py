808ce279de1a8025ccf037d53ca10cf3b5ee4e87
ADeogratias <34061333+ADeogratias@users.noreply.github.com>
2021-04-11 01:34:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pickle
 from nltk.stem import WordNetLemmatizer #reduce the word to a stem
 from tensorflow.keras.models import load_model 
 
-pip install nltk
+!pip install nltk
 import nltk
 nltk.download('punkt', quiet=True)
 nltk.download('averaged_perceptron_tagger')
